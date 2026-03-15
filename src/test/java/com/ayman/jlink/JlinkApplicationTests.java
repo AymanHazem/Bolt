619793809e297dd3e0b7543138cf670c4f4e9b3e
Ayman Hazem <aymooongsr2@gmail.com>
2026-03-15 15:48:15 +0200

    Initial commit (seed)

---
+++ src/test/java/com/ayman/jlink/JlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.jlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
